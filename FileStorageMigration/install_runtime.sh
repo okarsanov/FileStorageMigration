3903e384d015eed696c9eccbe07d4fbfcd64a56b
Alexei Kochetkov <kochetkov.aleksei2011@yandex.ru>
2021-08-11 08:53:44 +0300

    update install script

--- FileStorageMigration/install_runtime.sh
+++ FileStorageMigration/install_runtime.sh
@@ -1,8 +1,8 @@
-﻿apt-get install nano
+﻿apt-get update
+apt-get install nano
+apt-get install htop
 apt-get install wget
 wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
-apt-get update
 apt-get install -y apt-transport-https
-apt-get update
 apt-get install -y dotnet-sdk-5.0
